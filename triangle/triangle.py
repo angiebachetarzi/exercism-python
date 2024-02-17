9307e7b9cd43a920aa3f899349093b29f97d895c
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-17 17:25:51 +0100

    triangle: all tests pass

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,29 @@
 def equilateral(sides):
-    pass
+    occurences = occurences_array(sides)
+    return is_valid_triangle(sides) and 3 in occurences
 
 
 def isosceles(sides):
-    pass
+    occurences = occurences_array(sides) 
+    return is_valid_triangle(sides) and 2 in occurences or 3 in occurences
 
 
 def scalene(sides):
-    pass
+    occurences = occurences_array(sides) 
+    return is_valid_triangle(sides) and not 2 in occurences and not 3 in occurences
+
+
+def occurences_array(sides):
+    occurences = [0] * len(sides)
+    for i in range(len(sides)):
+        occurences[i] = sides.count(sides[i])
+    return occurences
+
+def is_valid_triangle(sides):
+    #a + b ≥ c
+    condition_1 = sides[0] + sides[1] >= sides[2]
+    #b + c ≥ a
+    condition_2 = sides[1] + sides[2] >= sides[0]
+    #a + c ≥ b
+    condition_3 = sides[0] + sides[2] >= sides[1]
+    return sides.count(0) == 0 and condition_1 and condition_2 and condition_3
