54f9094dcba22e6aebb657d83e88ff7c3926383b
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-17 18:11:11 +0100

    collatz-conjecture: all tests pass

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    if number <= 0: raise ValueError('Only positive integers are allowed')
+    return conjecture(number, 0)
+
+def conjecture(number, steps):
+    if number == 1: return steps
+    else:
+        if number % 2 == 0:
+            return conjecture(number//2, steps + 1)
+        else:
+            return conjecture(number * 3 + 1, steps + 1)
