abe9c69fcc320b5e8e4f96d871a78991f188bf41
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-17 17:44:59 +0100

    grains: all tests pass

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,11 @@
 def square(number):
-    pass
+    if number <= 0 or number > 64: raise ValueError('square must be between 1 and 64')
+    return pow(2, number - 1)
 
 
 def total():
-    pass
+    sum = 0
+    for i in range(1, 65):
+        print(i, square(i))
+        sum += square(i)
+    return sum
