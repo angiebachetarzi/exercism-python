52aab6f43fd4d8bd7a0114cdeb610e601e2c2ae3
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-22 15:32:09 +0100

    raindrops: all tests pass

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,14 @@
 def convert(number):
-    pass
+    div3 = number % 3 == 0
+    div5 = number % 5 == 0
+    div7 = number % 7 == 0
+    str_res = ''
+    if div3:
+        str_res += 'Pling'
+    if div5:
+        str_res += 'Plang'
+    if div7:
+        str_res += 'Plong'
+    if not str_res:
+        return str(number)
+    return str_res
