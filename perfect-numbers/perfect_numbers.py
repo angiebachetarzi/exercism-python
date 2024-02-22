25bff81962649fdfe3ed5c676e896dc2e6c55e93
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-22 17:34:59 +0100

    perfect-numbers: all tests pass

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -4,4 +4,15 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError('Classification is only possible for positive integers.') 
+    sum = 0
+    for i in range(1, number - 1):
+        if (number % i == 0):
+            sum += i
+    if sum == number:
+        return 'perfect'
+    if sum < number:
+        return 'deficient'
+    return 'abundant'
+
