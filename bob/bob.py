68c775f0c40f2acd8a26d45a6c00e1ff343f25ac
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-22 15:21:23 +0100

    bob: all tests pass

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,12 @@
 def response(hey_bob):
-    pass
+    message = hey_bob.strip()
+    if not message:
+        return "Fine. Be that way!"
+    if message.isupper():
+        if message[-1] == '?':
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
+    if message[-1] == '?':
+        return "Sure."
+    return "Whatever."
