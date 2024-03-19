bf81173abc2241cc0a67168d464f3dd2a7b206ad
Angie Bachetarzi <angiebachetarzi@Angies-Perso.local>
2024-03-20 00:08:07 +0100

    darts: done

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,13 @@
+import math
+
 def score(x, y):
-    pass
+    dist = math.sqrt(x**2 + y**2)
+    if dist > 10:
+        return 0
+    elif dist > 5:
+        return 1
+    elif dist > 1:
+        return 5
+    elif dist >= 0:
+        return 10
+    return -1
